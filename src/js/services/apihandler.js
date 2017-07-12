f672d2d93f4d9ead32ef2e80f2925a6f2448a3d4
Glauber Funez <glauber_funez@hotmail.com>
2017-07-12 00:35:52 -0300

    teste log

--- src/js/services/apihandler.js
+++ src/js/services/apihandler.js
@@ -225,7 +225,7 @@
             // return path && [apiUrl, $.param(data)].join('?');
             console.log('tt', path && [apiUrl, $.param(data)].join('?'));
             console.log('data path ', data.path);
-            console.log('url feita na mao', 'https://vimbo-gestao-ml/storage/' + window.localStorage.e + data.path);
+            console.log('url feita na mao', 'https://vimbo-gestao-ml/storage/empresas/' + window.localStorage.e + data.path);
             console.log('url', apiUrl);
             console.log('data obj ', data);
             return path && [apiUrl, $.param(data)].join('?');
