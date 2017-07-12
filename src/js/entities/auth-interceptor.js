ef38c8fe27b733e1c6eb985189d699b5093435bd
Glauber Funez <glauber_funez@hotmail.com>
2017-07-11 23:54:53 -0300

    teste log

--- src/js/entities/auth-interceptor.js
+++ src/js/entities/auth-interceptor.js
@@ -1,29 +1,41 @@
 (function() {
   'use strict';
 
-  angular.module('FileManagerApp')
-    .factory('authInterceptor', authInterceptor);
+  // angular.module('FileManagerApp')
+  //   .factory('authInterceptor', authInterceptor);
 
-  authInterceptor.$inject = ['$q', '$location'];
+  // authInterceptor.$inject = ['$q', '$location'];
 
-  function authInterceptor($q, $location) {
-    return {
-      request : function(config) {
-        config.headers = config.headers || {};
+  // function authInterceptor($q, $location) {
+  //   return {
+  //     request : function(config) {
+  //       config.headers = config.headers || {};
         
-        if (localStorage.getItem('vimbo_token')) {
-          config.headers.Authorization = 'Bearer ' + localStorage.getItem('vimbo_token');
-        }
-        console.log(config.headers);
-        return config;
-      }
-      // ,
-      // responseError : function(rejection) {
-      //   if (rejection.status === 401 || rejection.status === 403) {
-      //     $location.path('/erro/nao-autorizado');
-      //   }
-      //   return $q.reject(rejection);
-      // }
-    };
-  }
+  //       if (localStorage.getItem('vimbo_token')) {
+  //         config.headers.Authorization = 'Bearer ' + localStorage.getItem('vimbo_token');
+  //       }
+  //       console.log(config.headers);
+  //       return config;
+  //     }
+  //     ,
+  //     responseError : function(rejection) {
+  //       if (rejection.status === 401 || rejection.status === 403) {
+  //         $location.path('/erro/nao-autorizado');
+  //       }
+  //       return $q.reject(rejection);
+  //     }
+  //   };
+  // }
+  angular.module('FileManagerApp').factory('httpRequestInterceptor', function () {
+  return {
+    request: function (config) {
+      config.headers['Authorization'] = 'Bearer ' + localStorage.getItem('vimbo_token');
+      return config;
+    }
+  };
+});
+
+angular.module('FileManagerApp').config(function ($httpProvider) {
+  $httpProvider.interceptors.push('httpRequestInterceptor');
+});
 })();
