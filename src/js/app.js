beceb712f47c82ed99064da017f48f80689fb68f
Glauber Funez <glauber.funez@prosoftwares.net.br>
2018-09-10 16:33:50 -0300

    ajustes

--- src/js/app.js
+++ src/js/app.js
@@ -15,6 +15,8 @@
         $('#context-menu').hide();
     });
 
+    console.log('dentro do file manager app - testes')
+
     function getCordinates(e) {
         var m_posx = 0, m_posy = 0, e_posx = 0, e_posy = 0;
         if (!e) { e = window.event; }
